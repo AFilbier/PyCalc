0e546629ed1f96b37121a611e73dd379aac1bc75
Artur <velius1@wp.pl>
2017-11-01 17:43:58 +0100

    Upgraded letters-error handling

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 import re
 
 wynik = 0
-counter = 0
+
 loop = True
 
 #funkcja wyrzuca wiodace zera z liczb jezeli liczba nie jest zerem
@@ -22,55 +22,47 @@ def przeliczaj():
     skladowe = []
     bez_widacych_zer = []
 
-#zeby prompt byl tylko na poczatku, po resecie, po wyniku 0
-    if wynik == 0:
-        if counter == 0:
-            dzialanie = input("Type equation:\n")
-        if counter == 1:
-            dzialanie = input("Last one evaluated to zero, type next equation:\n")
 
-    else:
-        dzialanie = input(str(wynik))
+    dzialanie = input("Type equation:\n")
 
 #zabezpieczenie przed brakiem wprowadzenia
     if dzialanie == "":
         print("No input!")
-
 #wyjscie
     elif dzialanie == "quit":
         loop = False
-
-#reser
+#reset
     elif dzialanie == "c":
         wynik = 0
-        counter = 0
 
 #dodanie spacji przed/po liczbach zeby mozna je bylo potem pociac dla funkcji wywalania zer
     else:
-        counter = 1
+
         dzialanie = re.sub('(\\d+)', ' \\1 ', dzialanie)
 
 # wywalam wiodace zera jezeli gdzies sa
         skladowe = dzialanie.split()
-
-#        print(skladowe)
         bez_wiodacych_zer = list(map(wywal_zera, skladowe))
         dzialanie = ''.join(bez_wiodacych_zer)
 
 # wywalam litery, znaki specjalne itd
 
-        dzialanie = re.sub('[a-zA-Z,:%$#@!&^()=_" "]','', dzialanie)
+        dzialanie = re.sub('[a-zA-Z,:%$#@!&^=_" "]','', dzialanie)
 
-#jezel to kolejne dzialanie to przenies do niego wynik poprzedniego
-        if wynik == 0:
-            wynik = eval(dzialanie)
+        if dzialanie == "":
+            dzialanie = 0
+            wynik = 0
         else:
-            wynik = eval(str(wynik) + dzialanie)
+            wynik = eval(dzialanie)
+
+        print ("Result:", wynik)
 
 
-print("Basic semi fool-proof continuous calculator")
+
+print("Basic semi fool-proof single equation calculator")
 print("Type 'quit' to exit the program. Type 'c' to reset")
 print("Calculator will ignore letters, most special characters and will remove any leading zeroes from numbers")
+print(" \( \) works and can be used in equations")
 
 while loop:
 
