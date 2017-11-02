b21ca16f21211b8b2ab69d194dca55442e8c2c6f
Artur <velius1@wp.pl>
2017-11-02 17:50:38 +0100

    Added more consistent formatting

--- calc.py
+++ calc.py
@@ -1,21 +1,21 @@
 import re
 
+
 wynik = 0
 counter = 0
 loop = True
 
-#funkcja wyrzuca wiodace zera z liczb jezeli liczba nie jest zerem
 
+#funkcja wyrzuca wiodace zera z liczb jezeli liczba nie jest zerem
 def wywal_zera(element):
     if len(str(element)) > 1:
         return str(element).lstrip("0")
     else:
         return str(element)
 
-#glowna funckja do obliczen
 
+#Glowna fukncja do obliczen
 def przeliczaj():
-
     global wynik,counter,loop
 
     dzialanie = ""
@@ -28,7 +28,6 @@ def przeliczaj():
             dzialanie = input("Type equation:\n")
         if counter == 1:
             dzialanie = input("Last one evaluated to zero, type next equation:\n")
-
     else:
         dzialanie = input(str(wynik))
 
@@ -39,8 +38,7 @@ def przeliczaj():
 #wyjscie
     elif dzialanie == "quit":
         loop = False
-
-#reser
+#reset
     elif dzialanie == "c":
         wynik = 0
         counter = 0
@@ -52,13 +50,10 @@ def przeliczaj():
 
 # wywalam wiodace zera jezeli gdzies sa
         skladowe = dzialanie.split()
-
-#        print(skladowe)
         bez_wiodacych_zer = list(map(wywal_zera, skladowe))
         dzialanie = ''.join(bez_wiodacych_zer)
 
 # wywalam litery, znaki specjalne itd
-
         dzialanie = re.sub('[a-zA-Z,:%$#@!&^()=_" "]','', dzialanie)
 
 #jezel to kolejne dzialanie to przenies do niego wynik poprzedniego
@@ -68,7 +63,6 @@ def przeliczaj():
 #dodac sprawdzanie niepelnych obliczen
         else:
             wynik = eval(str(wynik) + dzialanie)
-
 
 print("Basic semi fool-proof continuous calculator")
 print("Type 'quit' to exit the program. Type 'c' to reset")
